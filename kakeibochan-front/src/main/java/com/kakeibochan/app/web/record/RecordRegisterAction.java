6ecda6f9f0cd47ed362a81ddf6eab1e9124e0121
SaayaMurakami <s-murakami@unext.jp>
2018-07-31 20:32:20 +0900

    明細のページングを実装（完了）

--- kakeibochan-front/src/main/java/com/kakeibochan/app/web/record/RecordRegisterAction.java
+++ kakeibochan-front/src/main/java/com/kakeibochan/app/web/record/RecordRegisterAction.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import javax.annotation.Resource;
 
 import org.dbflute.cbean.result.ListResultBean;
-import org.dbflute.optional.OptionalThing;
 import org.lastaflute.web.Execute;
 import org.lastaflute.web.response.HtmlResponse;
 
@@ -28,7 +27,7 @@ public class RecordRegisterAction extends FrontBaseAction {
     private AssetBhv assetBhv;
 
     @Execute
-    public HtmlResponse index(OptionalThing<String> categoryType) {
+    public HtmlResponse index(categoryType) {
         return renderIndex();
     }
 
